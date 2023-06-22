c5cdf856eebf72124979bd90ef6db0317db2c348
Rivalzy <rivalds011@gmail.com>
2023-06-22 07:02:12 +0700

    edit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const calculateTemp = () => {
+const calculateTemp = () => { // arrow function untuk mengesekusi temp yang sudah dimasukkan
   const tempSelected = document.getElementById("temp_diff");
   const valueTemp = temp_diff.options[tempSelected.selectedIndex].value;
 
   // Celsius to Fahrenheit
   const celToFah = (cel) => {
-    let fahrenheit = ((cel * 9) / 5 + 32).toFixed(1);
+    let fahrenheit = ((cel * 9) / 5 + 32).toFixed(1); // to.Fixed digunakan untuk memberikan 1 angka dibelakang desimal
     return fahrenheit;
   };
 
@@ -20,8 +20,9 @@ const calculateTemp = () => {
 
     document.getElementById("fahrenheitInput").value = celToFah(inputTemp);
 
+    // caraKalkuasi menampilkan pada laman interface
     const caraKalkulasi = document.getElementById("cara");
-    caraKalkulasi.innerHTML = `${inputTemp}&#176;C * ( 9/5 ) + 32 = ${celToFah(
+    caraKalkulasi.innerHTML = `(${inputTemp}&#176;C x 9/5 ) + 32 = ${celToFah(
       inputTemp
     )}&#176;F`;
   } else {
@@ -31,9 +32,19 @@ const calculateTemp = () => {
     document.getElementById("celsiusInput").value = fahToCel(inputTemp);
 
     const caraKalkulasi = document.getElementById("cara");
-    caraKalkulasi.innerHTML = `(${inputTemp}&#176;F - 32) * 5/9 = ${fahToCel(
+    caraKalkulasi.innerHTML = `(${inputTemp}&#176;F - 32) x 5 ) / 9 = ${fahToCel(
       inputTemp
     )}&#176;C`;
   }
 };
+
+// Menambahkan fungsi resetForm() untuk menangani klik pada tombol reset
+function resetForm() {
+  console.log("Berhasil dihapus")
+  // Mengatur nilai input menjadi 0
+  document.getElementById("celsiusInput").value = "";
+  document.getElementById("fahrenheitInput").value = "";
+  // Menghapus kalkulasi yang ditampilkan
+  document.getElementById("cara").innerHTML = "";
+}
 
